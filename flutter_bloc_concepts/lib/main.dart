66ffab2600fe2a276d4572b774649a88948900d6
dxps <marius.ileana@gmail.com>
2021-07-10 12:48:45 +0300

    flutter_bloc_concepts | BlockProvider in home not whole app, using BlocConsumer instead of BlocListener

--- flutter_bloc_concepts/lib/main.dart
+++ flutter_bloc_concepts/lib/main.dart
@@ -9,12 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
-        title: 'Flutter Bloc Concepts',
-        theme: ThemeData(primarySwatch: Colors.blueGrey, visualDensity: VisualDensity.adaptivePlatformDensity),
-        home: MyHomePage(title: 'Flutter Bloc Concepts'),
+    return MaterialApp(
+      title: 'Flutter Bloc Concepts',
+      theme: ThemeData(primarySwatch: Colors.blueGrey, visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: BlocProvider<CounterCubit>(
+        create: (context) => CounterCubit(),
+        child: MyHomePage(title: 'Flutter Bloc Concepts'),
       ),
     );
   }
@@ -36,44 +36,44 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          if (state.wasIncremented) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Incremented!'), duration: Duration(milliseconds: 300)),
-            );
-          }
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('You have pushed the button this many times:'),
-              BlocBuilder<CounterCubit, CounterState>(
-                builder: (context, state) {
-                  return Text(state.counterValue.toString(), style: Theme.of(context).textTheme.headline4);
-                },
-              ),
-              SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FloatingActionButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).decrement();
-                      },
-                      tooltip: 'Decrement',
-                      child: Icon(Icons.remove)),
-                  FloatingActionButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).increment();
-                      },
-                      tooltip: 'Increment',
-                      child: Icon(Icons.add)),
-                ],
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              builder: (context, state) {
+                return Text(state.counterValue.toString(), style: Theme.of(context).textTheme.headline4);
+              },
+              listener: (context, state) {
+                if (state.wasIncremented) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Incremented!'), duration: Duration(milliseconds: 300)),
+                  );
+                }
+              },
+            ),
+            SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                    },
+                    tooltip: 'Decrement',
+                    child: Icon(Icons.remove)),
+                FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                    },
+                    tooltip: 'Increment',
+                    child: Icon(Icons.add)),
+              ],
+            ),
+          ],
         ),
       ),
     );
