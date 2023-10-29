f1e74ded131533c059c89b93d42ac94da9f74531
mileana <marius.ileana@tradeville.ro>
2023-10-29 12:14:38 +0200

    chore(flutter_stacked_web_library): small cleanup on home view desktop

--- flutter_stacked_web_library/lib/ui/views/home/home_view.desktop.dart
+++ flutter_stacked_web_library/lib/ui/views/home/home_view.desktop.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_stacked_web_library/ui/common/app_colors.dart';
 import 'package:flutter_stacked_web_library/ui/common/app_constants.dart';
-import 'package:flutter_stacked_web_library/ui/common/shared_styles.dart';
 import 'package:flutter_stacked_web_library/ui/common/ui_helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stacked_web_library/ui/views/home/widgets/home_image.dart';
@@ -9,7 +8,6 @@ import 'package:flutter_stacked_web_library/ui/views/home/widgets/home_title.dar
 import 'package:flutter_stacked_web_library/ui/widgets/common/google_sign_in.dart';
 import 'package:flutter_stacked_web_library/ui/widgets/common/library_icon.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:simple_gradient_text/simple_gradient_text.dart';
 import 'package:stacked/stacked.dart';
 
 import 'home_viewmodel.dart';
